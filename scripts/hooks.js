fc28d797456335fac5f31c8006e27e2008230ad0
cadowtin <chasdowtin@gmail.com>
2024-08-12 01:39:31 -0500

    Refactor some code to Hooks

---
+++ scripts/hooks.js
@@ -0,0 +1,81 @@
+import { CONDITIONS, MODULE_ID } from "./misc.js";
+import {
+  flashColor,
+  getAnimationChanges,
+  handleTokenHealthUpdate,
+} from "./module.js";
+import {
+  getHealingInfo,
+  getHealthLevel,
+  updateHasAllianceChange,
+} from "./systemCompatability.js";
+
+export async function preUpdateActor(actor, update, status, _userID) {
+  if (!status.diff) return;
+  const { isHeal, dmg, maxHP } = getHealingInfo(actor, update, status);
+  if (isHeal !== undefined) {
+    const healthLevel = getHealthLevel(actor, update);
+    // Put process into async subfunction so we can run it in parallel on multiple tokens
+    const processToken = async (token) => {
+      // Update health level flag for the module so the Ring color can be determined before the completed actor update
+      // This flag is synched over the network and can only be set by owners with permission.
+      await token.document.setFlag(MODULE_ID, "tokenHealthLevel", healthLevel);
+      // Run our visual fanciness. Run local function first, since it is not broadcast to ourselves
+      const args = [token.id, isHeal, dmg, maxHP];
+      handleTokenHealthUpdate(...args);
+      // Trigger other clients now
+      game.socket.emit(`module.${MODULE_ID}`, {
+        type: "tokenHealthUpdate",
+        payload: args,
+      });
+    };
+    for (const token of actor.getActiveTokens()) {
+      processToken(token);
+    }
+  }
+}
+
+export function updateActor(actor, update, status, _userID) {
+  if (!status.diff) return;
+  // Cause a manual token ring update if alliance changed via system, so it picks up disposition
+  // If the system-specific part is not implemented, it will simply synch up on the next regular update.
+  if (updateHasAllianceChange(actor, update)) {
+    for (const token of actor.getActiveTokens()) {
+      if (token.document.ring?.enabled) token.ring.configureVisuals();
+    }
+  }
+}
+
+export async function targetToken(user, token, isTargetting) {
+  if (
+    isTargetting &&
+    (user.id === game.user.id ||
+      game.settings.get(MODULE_ID, "target.share-flash"))
+  ) {
+    const color = game.settings.get(MODULE_ID, "target.player-color")
+      ? user.color.css
+      : game.settings.get(MODULE_ID, "colors.target");
+    await flashColor(token, color, getAnimationChanges("target", { token }));
+  }
+}
+
+export async function applyTokenStatusEffect(token, name, _unknown) {
+  const conditions = token.actor.conditions.active.map(
+    (c) => c?.rollOptionSlug
+  );
+
+  if (conditions.includes(name)) {
+    //is added
+    if (CONDITIONS.POSITIVE.includes(name)) {
+      await flashColor(
+        token,
+        game.settings.get(MODULE_ID, "colors.status.positive")
+      );
+    } else if (CONDITIONS.NEGATIVE.includes(name)) {
+      await flashColor(
+        token,
+        game.settings.get(MODULE_ID, "colors.status.negative")
+      );
+    }
+  }
+}
--- scripts/module.js
+++ scripts/module.js
@@ -1,12 +1,8 @@
 // Import necessary modules and constants
 import { autoColorRing } from "./autoColorRing.js";
-import { COLORS, CONDITIONS, MODULE_ID } from "./misc.js";
+import { preUpdateActor, targetToken, updateActor } from "./hooks.js";
+import { MODULE_ID } from "./misc.js";
 import { registerSettings } from "./settings.js";
-import {
-  getHealingInfo,
-  getHealthLevel,
-  updateHasAllianceChange,
-} from "./systemCompatability.js";
 
 // Initialize module settings and ring color wrapper
 Hooks.once("init", () => {
@@ -18,85 +14,21 @@ Hooks.once("init", () => {
 Hooks.once("ready", async () => {
   // Register the listener for our module socket.
   // Note that this is not entered on the origin client of any broadcast.
-  game.socket.on(`module.${MODULE_ID}`, ({type, payload}) => {
-    if (type === "tokenHealthUpdate")
-      handleTokenHealthUpdate(...payload);
+  game.socket.on(`module.${MODULE_ID}`, ({ type, payload }) => {
+    if (type === "tokenHealthUpdate") handleTokenHealthUpdate(...payload);
   });
 
   // Handle actor pre update, which is running only on the causing client
-  Hooks.on("preUpdateActor", async (actor, update, status, _userID) => {
-    if (!status.diff) return;
-    const { isHeal, dmg, maxHP } = getHealingInfo(actor, update, status);
-    if (isHeal !== undefined) {
-      const healthLevel = getHealthLevel(actor, update);
-      // Put process into async subfunction so we can run it in parallel on multiple tokens
-      const processToken = async (token) => {
-        // Update health level flag for the module so the Ring color can be determined before the completed actor update
-        // This flag is synched over the network and can only be set by owners with permission.
-        await token.document.setFlag(
-          MODULE_ID,
-          "tokenHealthLevel",
-          healthLevel
-        );
-        // Run our visual fanciness. Run local function first, since it is not broadcast to ourselves
-        const args = [token.id, isHeal, dmg, maxHP];
-        handleTokenHealthUpdate(...args);
-        // Trigger other clients now
-        game.socket.emit(`module.${MODULE_ID}`, {type: "tokenHealthUpdate", payload: args});
-      };
-      for (const token of actor.getActiveTokens()) {
-        processToken(token);
-      }
-    }
-  });
+  Hooks.on("preUpdateActor", preUpdateActor);
 
-  Hooks.on("updateActor", (actor, update, status, _userID) => {
-    if (!status.diff) return;
-    // Cause a manual token ring update if alliance changed via system, so it picks up disposition
-    // If the system-specific part is not implemented, it will simply synch up on the next regular update.
-    if (updateHasAllianceChange(actor, update)) {
-      for (const token of actor.getActiveTokens()) {
-        if (token.document.ring?.enabled) token.ring.configureVisuals();
-      }
-    }
-  });
+  Hooks.on("updateActor", updateActor);
 
   // Handle token targeting
-  Hooks.on("targetToken", async (user, token, isTargetting) => {
-    if (
-      isTargetting &&
-      (user.id === game.user.id ||
-        game.settings.get(MODULE_ID, "target.share-flash"))
-    ) {
-      const color = game.settings.get(MODULE_ID, "target.player-color")
-        ? user.color.css
-        : game.settings.get(MODULE_ID, "colors.target");
-      await flashColor(token, color, getAnimationChanges("target", { token }));
-    }
-  });
+  Hooks.on("targetToken", targetToken);
 
   // Handle PF2e specific condition changes
   if (game.system.id === "pf2e") {
-    Hooks.on("applyTokenStatusEffect", async (token, name, _unknown) => {
-      const conditions = token.actor.conditions.active.map(
-        (c) => c?.rollOptionSlug
-      );
-
-      if (conditions.includes(name)) {
-        //is added
-        if (CONDITIONS.POSITIVE.includes(name)) {
-          await flashColor(
-            token,
-            game.settings.get(MODULE_ID, "colors.status.positive")
-          );
-        } else if (CONDITIONS.NEGATIVE.includes(name)) {
-          await flashColor(
-            token,
-            game.settings.get(MODULE_ID, "colors.status.negative")
-          );
-        }
-      }
-    });
+    Hooks.on("applyTokenStatusEffect", applyTokenStatusEffect);
   }
 });
 
@@ -108,7 +40,7 @@ Hooks.once("ready", async () => {
  * @param {number} dmg
  * @param {number} maxHP
  */
-function handleTokenHealthUpdate(tokenId, isHeal, dmg, maxHP) {
+export function handleTokenHealthUpdate(tokenId, isHeal, dmg, maxHP) {
   const token = canvas.tokens.get(tokenId);
   if (!token) return;
   // Force updating of the ring elements including background before flashing
@@ -118,11 +50,7 @@ function handleTokenHealthUpdate(tokenId, isHeal, dmg, maxHP) {
     ? game.settings.get(MODULE_ID, "colors.healing")
     : game.settings.get(MODULE_ID, "colors.damage");
   const situation = isHeal ? "heal" : "damage";
-  flashColor(
-    token,
-    color,
-    getAnimationChanges(situation, { dmg, maxHP })
-  );
+  flashColor(token, color, getAnimationChanges(situation, { dmg, maxHP }));
 }
 
 /**
@@ -131,7 +59,7 @@ function handleTokenHealthUpdate(tokenId, isHeal, dmg, maxHP) {
  * @param {string} color - Color as hex (e.g., '#0000FF')
  * @param {Object} animationOverride - Override to animation options
  */
-async function flashColor(token, color, animationOverride = {}) {
+export async function flashColor(token, color, animationOverride = {}) {
   if (token?.document?.ring?.enabled) {
     const defaultAnimationOptions = {
       duration: game.settings.get(MODULE_ID, "duration"),
@@ -152,7 +80,7 @@ async function flashColor(token, color, animationOverride = {}) {
  * @param {Object} data - Additional data for the animation
  * @returns {Object} Animation changes
  */
-function getAnimationChanges(situation, { dmg, maxHP }) {
+export function getAnimationChanges(situation, { dmg, maxHP }) {
   const baseDuration = game.settings.get(MODULE_ID, "duration");
   const result = {};
 
