900b58c97e6d727ed037401b2ae415eeb85633fa
cadowtin <chasdowtin@gmail.com>
2024-08-16 16:04:16 -0500

    Fix localization for import/export settings

--- scripts/settings.js
+++ scripts/settings.js
@@ -211,7 +211,9 @@ export function renderSettingsConfig(_, html) {
     <div class="REDY-button-container">
       <button type="button" class="REDY-button-settings" onclick="(async () => { 
         game.REDY.api.${action}Settings(${action === "export" ? true : ""}); 
-      })()">${action} Settings
+      })()">${game.i18n.localize(
+        `${MODULE_ID}.module-settings.import-export-menu.${action}-settings`
+      )} Settings
       </button>
     </div>`
   );
